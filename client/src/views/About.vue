6c68412edf7994fe9d81e418126896a97eb024f8
tsukumi <tsukumijima@users.noreply.github.com>
2024-06-11 18:13:11 +0900

    Update: [Client] About ページの内容を更新

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -8,6 +8,7 @@
                 <p class="mt-4 text-text-darken-1">
                     <strong>NX-Jikkyo は、<a class="link" href="https://blog.nicovideo.jp/niconews/225099.html" target="_blank">サイバー攻撃で期間未定の鯖落ち中</a> のニコニコ実況に代わる、ニコニコ実況民のための避難所であり、<a class="link" href="https://github.com/tsukumijima/NX-Jikkyo/blob/master/server/app/routers/comments.py" target="_blank">ニコニコ生放送互換の WebSocket API</a> を備えるコメントサーバーです。</strong><br>
                 </p>
+                <hr class="mt-5">
                 <p class="mt-5 text-text-darken-1">
                     個人的にもニコニコ実況がすぐに復活してくれればそれが一番良いのですが、この感じだと残念ながら数週間はサーバーダウンが続きそうに思えます。<br>
                     2020年12月までのニコニコ実況は独立していたのですが、<a class="link" href="https://blog.nicovideo.jp/niconews/143148.html" target="_blank">Adobe Flash 廃止に伴いニコ生のチャンネル生放送として統合されてしまいました。</a><br>
@@ -17,12 +18,17 @@
                     ニコニコ実況が使えない時間が続けば続くほど、リアルタイムに実況できないのはもちろんのこと、録画でニコニコ実況の過去ログを見て作品を楽しむライフワークもできなくなってしまいます。<br>
                     3日程度ならともかく、数週間続く可能性を鑑みるとかなり受け入れ難いものです。
                 </p>
+                <hr class="mt-5">
                 <p class="mt-5 text-text-darken-1">
-                    <strong>そこで突貫工事ではありますが、ニコニコ生放送互換のサードパーティーツールが比較的対応しやすい技術仕様で、ニコニコ実況が使えない間のつなぎとしてテレビを実況できる、このサイトを開発しました。</strong>ソースコードは <a class="link" href="https://github.com/tsukumijima/NX-Jikkyo" target="_blank">GitHub</a> で公開しています。<br>
+                    <strong>そこで突貫工事ではありますが、ニコニコ生放送互換のサードパーティーツールが比較的対応しやすい技術仕様で、ニコニコ実況が使えない間のつなぎとしてテレビを実況できる、このサイトを開発しました。</strong><br>
                 </p>
                 <blockquote class="mt-5 text-text-darken-1">
                     <strong>ぜひこのサイトをまだ知らないニコニコ実況難民の方に広めていただけると嬉しいです！</strong><br>
                     コメントサーバーの負荷問題は……なんとかします…！
+                </blockquote>
+                <blockquote class="mt-5 text-text-darken-1">
+                    ソースコードは <a class="link" href="https://github.com/tsukumijima/NX-Jikkyo" target="_blank">GitHub</a> で公開しています。<a class="link" href="/api/v1/docs" target="_blank">API ドキュメント</a> もあります。<br>
+                    WebSocket API のドキュメントは <a class="link" href="https://github.com/tiangolo/fastapi" target="_blank">FastAPI</a> が API ドキュメントを自動生成してくれないため現状ありませんが、ニコ生の WebSocket API のドロップイン代替として機能するはずです。<br>
                 </blockquote>
                 <blockquote class="mt-5 text-text-darken-1">
                     勘の良い方はおそらくお気づきの通り、このサイトは私が長年開発している <a class="link" href="https://github.com/tsukumijima/KonomiTV" target="_blank">KonomiTV</a> の大半のソースコードを流用して開発しています。<br>
@@ -38,6 +44,7 @@
                     またシステム簡素化のため、意図的にアカウント不要で書き込めるようにしています（データベースに個人情報が保存されることは絶対にありません）。<br>
                     気軽にお使いいただけますが、マナーを守ってのご利用をお願いします。
                 </p>
+                <hr class="mt-5">
                 <p class="mt-5 text-text-darken-1">
                     <strong>このサイトを公開した最大の理由は、十数年にも及ぶニコニコ実況の歴史上異常事態である、数週間に渡りテレビの過去ログコメントが完全に断たれる事態をなんとしてでも避けたいからです。</strong><br>
                     もちろん15時間で突貫で作ったサイトなのでバグも多いでしょうし、大量のコメントの負荷には耐えきれないかもしれません。しかし、コメントが全く残らないよりはマシだと考えています。
@@ -50,7 +57,8 @@
                     もう少しかっこいい名前が出てくれば良かったのですが、「Jikkyo」と入れないと何のサービスか分かりづらそうというのもあり…。
                 </blockquote>
                 <p class="mt-5 text-text-darken-1 text-right">
-                    2024/06/10 <a class="link" href="https://blog.tsukumijima.net" target="_blank">tsukumi</a> (<a class="link" href="https://twitter.com/TVRemotePlus" target="_blank">Twitter@TVRemotePlus</a>)
+                    2024/06/10 (Last Update: 2024/06/11)<br>
+                    <a class="link" href="https://blog.tsukumijima.net" target="_blank">tsukumi</a> (<a class="link" href="https://twitter.com/TVRemotePlus" target="_blank">Twitter@TVRemotePlus</a>)
                 </p>
             </div>
         </main>
@@ -65,8 +73,11 @@ import Navigation from '@/components/Navigation.vue';
 <style lang="scss" scoped>
 
 blockquote {
-    border-left: 2px solid #e5e5d1;
+    border-left: 3px solid rgb(var(--v-theme-text));
     padding-left: 12px;
+}
+hr {
+    border-color: rgb(var(--v-theme-text-darken-2));
 }
 
 </style>
