8d9d3cbd821c03cd87144458a830ad38d8d5ceaf
tsukumi <tsukumijima@users.noreply.github.com>
2024-06-12 03:50:38 +0900

    Fix: [Client] 細かな修正

--- client/src/components/Watch/Player.vue
+++ client/src/components/Watch/Player.vue
@@ -546,7 +546,7 @@ _::-webkit-full-page-media, _:future, :root .dplayer-icon:hover .dplayer-icon-co
                 line-height: 1.5;
                 text-align: center;
                 white-space: pre;
-                filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.4));
+                filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.5));
 
                 @include smartphone-vertical {
                     font-size: 24px;
