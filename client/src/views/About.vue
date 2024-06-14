5f3a68737807c18e8406878b6bfa104fc5b88e02
tsukumi <tsukumijima@users.noreply.github.com>
2024-06-14 16:44:54 +0900

    Update: [Client] About ページの内容を更新

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -6,7 +6,7 @@
             <div class="px-5 py-8" style="width: 100%; max-width: 850px; margin: 0 auto; line-height: 1.65;">
                 <h1>NX-Jikkyo とは</h1>
                 <p class="mt-4 text-text-darken-1">
-                    <strong>NX-Jikkyo は、<a class="link" href="https://blog.nicovideo.jp/niconews/225099.html" target="_blank">サイバー攻撃で期間未定の鯖落ち中</a> のニコニコ実況に代わる、ニコニコ実況民のための避難所であり、<a class="link" href="https://github.com/tsukumijima/NX-Jikkyo/blob/master/server/app/routers/websocket.py" target="_blank">ニコニコ生放送互換の WebSocket API</a> を備えるコメントサーバーです。</strong><br>
+                    <strong>NX-Jikkyo は、<a class="link" href="https://blog.nicovideo.jp/niconews/225099.html" target="_blank">サイバー攻撃で最低7月末まで鯖落ち中</a> のニコニコ実況に代わる、ニコニコ実況民のための避難所であり、<a class="link" href="https://github.com/tsukumijima/NX-Jikkyo/blob/master/server/app/routers/websocket.py" target="_blank">ニコニコ生放送互換の WebSocket API</a> を備えるコメントサーバーです。</strong><br>
                 </p>
                 <blockquote class="mt-5 text-text-darken-1">
                     <strong><a class="link" href="https://twitter.com/TVRemotePlus" target="_blank">Twitter@TVRemotePlus</a> やハッシュタグ <a class="link" href="https://x.com/search?q=%23NXJikkyo&src=typed_query" target="_blank">#NXJikkyo</a> では NX-Jikkyo の最新情報を発信しています！<br>ぜひチェックしてみてください🙏</strong>
@@ -18,17 +18,17 @@
                 <blockquote class="mt-5 text-text-darken-1">
                     <strong><a class="link" href="https://github.com/DBCTRADO/TVTest" target="_blank">TVTest</a> のニコニコ実況表示プラグイン <a class="link" href="https://github.com/xtne6f/NicoJK/releases/tag/master-240613" target="_blank">NicoJK master-240613</a> にて、ニコニコ実況の代わりに NX-Jikkyo からリアルタイムに実況コメントを取得する設定が追加されました！<br></strong>
                     <a class="link" href="https://github.com/DBCTRADO/TVTest" target="_blank">TVTest</a> + ニコニコ実況ユーザーの方は NicoJK.ini を編集して NX-Jikkyo を有効にすると再び実況コメントを表示できるようになるのでアプデ推奨です🙏<br>
-                    NicoJK の更新方法の詳細は <a class="link" href="https://blog.tsukumijima.net/article/nx-jikkyo-released/" target="_blank">こちら</a> に記載しています。
+                    <strong>NicoJK の更新方法の詳細は <a class="link" href="https://blog.tsukumijima.net/article/nx-jikkyo-released/#toc3" target="_blank">こちら</a> の記事に載せていますのでぜひ一読を🙇</strong>
                 </blockquote>
                 <h2 class="mt-5">開発経緯</h2>
                 <p class="mt-3 text-text-darken-1">
-                    個人的にもニコニコ実況がすぐに復活してくれればそれが一番良いのですが、この感じだと残念ながら数週間はサーバーダウンが続きそうに思えます。<br>
+                    個人的にもニコニコ実況がすぐに復活してくれればそれが一番良かったのですが、<strong>2024/06/14 時点での <a class="link" href="https://blog.nicovideo.jp/niconews/225099.html" target="_blank">ニコニコインフォの報告</a> によれば、ニコニコ実況 (ニコニコ生放送公式チャンネル) の復旧は最短でも8月以降になりそうな極めて厳しい状況です。</strong><br>
                     2020年12月までのニコニコ実況は独立していたのですが、<a class="link" href="https://blog.nicovideo.jp/niconews/143148.html" target="_blank">Adobe Flash 廃止に伴いニコ生のチャンネル生放送として統合されてしまいました。</a><br>
-                    よってニコ生本体が完全復旧しない限り、ニコニコ実況も当分復旧しそうにない状況です。
+                    よってニコ生本体が完全復旧しない限り、ニコニコ実況も当分復旧しそうにありません。<br>
                 </p>
                 <p class="mt-2 text-text-darken-1">
                     ニコニコ実況が使えない時間が続けば続くほど、リアルタイムに実況できないのはもちろんのこと、録画でニコニコ実況の過去ログを見て作品を楽しむライフワークもできなくなってしまいます。<br>
-                    3日程度ならともかく、数週間続く可能性を鑑みるとかなり受け入れ難いものです。
+                    1ヶ月以上もテレビを実況できない生活は相当に受け入れ難いものです。
                 </p>
                 <p class="mt-5 text-text-darken-1">
                     <strong>そこで突貫工事ではありますが、ニコニコ生放送互換のサードパーティーツールが比較的対応しやすい技術仕様で、ニコニコ実況が使えない間のつなぎとしてテレビを実況できる、このサイトを開発しました。</strong><br>
@@ -60,7 +60,7 @@
                 </blockquote>
                 <hr class="mt-5">
                 <p class="mt-5 text-text-darken-1">
-                    <strong>このサイトを公開した最大の理由は、十数年にも及ぶニコニコ実況の歴史上異常事態である、数週間に渡りテレビの過去ログコメントが完全に断たれる事態をなんとしてでも避けたいからです。</strong><br>
+                    <strong>このサイトを公開した最大の理由は、十数年にも及ぶニコニコ実況の歴史上異常事態である、1ヶ月以上に渡りテレビの過去ログコメントが完全に断たれる事態をなんとしてでも避けたいからです。</strong><br>
                     もちろん元々15時間で突貫で作ったサイトなのでバグも多いでしょうし、大量のコメントの負荷には耐えきれないかもしれません（随時改善中です）。しかし、コメントが全く残らないよりはマシだと考えています。
                 </p>
                 <p class="mt-2 text-text-darken-1">
