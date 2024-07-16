0a8c9a895244718950298c1744dfc83d8b7f6117
tsukumi <tsukumijima@users.noreply.github.com>
2024-07-16 17:56:51 +0900

    Update: [Client] ヘッダーを更新

--- client/src/views/TV/Home.vue
+++ client/src/views/TV/Home.vue
@@ -5,13 +5,13 @@
             <Navigation />
             <div class="channels-container channels-container--home" :class="{'channels-container--loading': is_loading}">
                 <div class="mt-5 mb-2 mx-4 text-center font-weight-bold" style="font-size: 15px">
-                    <blockquote class="text-left mx-auto" style="max-width: 850px">
+                    <blockquote class="text-left mx-auto" style="max-width: 720px">
                         【重要なお知らせ】ニコニコ公式から「仮復旧」と称して <a class="link" href="https://originalnews.nico/464285" target="_blank">ニコニコ実況(Re:仮)</a> がリリースされましたが、<br>
                         <a class="link" href="https://x.com/TVRemotePlus/status/1813121242565656577" target="_blank">
                         「過去ログが残らず古いコメントから消えていく」<br class="d-inline d-sm-none">
                         「技術的にニコ生との API 互換性が皆無」<br>
                         「コメント書き込み時に Captcha が入ってるため TVTest などのサードパーティーソフトで対応不可」<br>
-                        な使い捨ての劣化版クソサービス</a> なので、何卒引き続きこの NX-Jikkyo 避難所でのコメントをお願いいたします…🙏🙏
+                        「当然 NHK BS・BS11 以外の BS チャンネルには非対応」な使い捨ての劣化版クソサービス</a> なので、<br>何卒引き続きこの NX-Jikkyo 避難所でのコメントをお願いいたします…🙏🙏
                     </blockquote>
                     <div class="mt-2">ぜひこのサイトをまだ NX-Jikkyo を知らないニコニコ実況難民の方に広めていただけると嬉しいです！</div>
                     <div class="mt-1"><a class="link" href="https://air.fem.jp/jkcommentviewer/" target="_blank">jkcommentviewer</a> / <a class="link" href="https://blog.tsukumijima.net/article/nx-jikkyo-released/#toc4" target="_blank">TVTest (NicoJK)</a> / <a class="link" href="https://github.com/tsukumijima/KonomiTV/releases/tag/v0.10.1" target="_blank">KonomiTV</a> 最新版で NX-Jikkyo に対応しました！！🎉🎊</div>
